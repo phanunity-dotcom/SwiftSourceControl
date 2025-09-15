e2d04e913784ec5d2c6753e8eae04d5d561027b7
hp-code <phan.unity@gmail.com>
2025-09-15 11:41:41 +0700

    Initial Commit

---
+++ SwiftSourceControl/SwiftSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlApp.swift
+//  SwiftSourceControl
+//
+//  Created by HP on 15/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftSourceControlTests/SwiftSourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlTests.swift
+//  SwiftSourceControlTests
+//
+//  Created by HP on 15/9/25.
+//
+
+import Testing
+@testable import SwiftSourceControl
+
+struct SwiftSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
